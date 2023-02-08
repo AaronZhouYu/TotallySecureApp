3b4a262afe0ae16e0d7f51169adcf440d43c6955
Zhou Yu, Aaron <aaron.zhouyu@gmail.com>
2023-02-08 10:47:32 +0800

    Set equire_uppercase_characters = true in terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,6 +1,6 @@
 resource "alicloud_ram_account_password_policy" "corporate1" {
   require_lowercase_characters = false
-  require_uppercase_characters = false
+  require_uppercase_characters = true
   require_numbers              = false
   require_symbols              = false
   hard_expiry                  = true
